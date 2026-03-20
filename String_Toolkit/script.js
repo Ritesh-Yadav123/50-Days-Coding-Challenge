153341c32157621d7335d2a1a028597b6fd2d338
Ritesh <riteshyadav825410@gmail.com>
2026-03-20 17:15:56 +0530

    completed string toolkit

--- String_Toolkit/script.js
+++ String_Toolkit/script.js
@@ -7,7 +7,7 @@ let capitalizeWords = document.getElementById("capitalizeWords");
 let clearText = document.getElementById("clearText");
 let inputString = document.getElementById("textInput");
 
-let resutl = document.getElementById("result");
+let result = document.getElementById("result");
 
 inputString.addEventListener("input", function() {
      result.innerHTML="you entered: " + inputString.value;
@@ -20,3 +20,40 @@ inputString.addEventListener("focus", function() {
 inputString.addEventListener("blur", function() {
     result.innerHTML="We have finished";
 });
+
+countWord.addEventListener('click',countWordcheck)
+function countWordcheck(){
+     let text=inputString.value.trim();
+     if(text==""){
+          result.innerHTML="Please enter a string";
+     }
+     result.innerHTML=text.length;
+}
+
+reverseString.addEventListener('click',reverseStringcheck);
+function reverseStringcheck(){
+     let text=inputString.value.trim();
+     result.innerHTML="";
+     for(let i=text.length-1;i>=0;i--){
+          result.innerHTML+=text[i];
+     }
+}
+
+toUpperCaseBtn.addEventListener('click',toUpperCasecheck);
+function toUpperCasecheck(){
+     let text=inputString.value.trim();
+     result.innerHTML=text.toUpperCase();
+}
+
+
+toLowerCaseBtn.addEventListener('click',toLowerCasecheck);
+function toLowerCasecheck(){
+     let text=inputString.value.trim();
+     result.innerHTML=text.toLowerCase();
+}
+
+clearText.addEventListener('click',resetString);
+function resetString(){
+     let text=inputString.value.trim();
+     result.innerHTML="";
+}
