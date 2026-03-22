a2a9ec1ccbbcc341b442691d3322d79bea6061ea
Ritesh <riteshyadav825410@gmail.com>
2026-03-22 23:33:28 +0530

    added array

--- ArrayOperations/script.js
+++ ArrayOperations/script.js
@@ -1,6 +1,10 @@
 arr=document.getElementById("arrayDisplay");
+arrayDisplayPlace=document.getElementById("arrayDisplay")
 
 (function(){
     array=prompt("Enter the arrays elements separted by ',' ");
     arr.innerHTML=array.split(",").join();
 })();
+
+console.log(array[3])
+console.log(type(array))
