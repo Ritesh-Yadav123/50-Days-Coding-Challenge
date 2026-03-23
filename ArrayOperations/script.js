98420c4c8d4f8dcd5637c3d4aa3823fd68ca55d7
Ritesh <riteshyadav825410@gmail.com>
2026-03-23 23:39:44 +0530

    completed js and html parts

--- ArrayOperations/script.js
+++ ArrayOperations/script.js
@@ -1,10 +1,51 @@
-arr=document.getElementById("arrayDisplay");
-arrayDisplayPlace=document.getElementById("arrayDisplay")
+arrDisplay=document.getElementById("arrayDisplay");
+maxiBtn=document.querySelector("#max");
+miniBtn=document.querySelector("#min");
+dRbutton=document.querySelector("#removeDuplicate");
 
+let array=[];
 (function(){
-    array=prompt("Enter the arrays elements separted by ',' ");
-    arr.innerHTML=array.split(",").join();
+    input=prompt("Enter the arrays elements separted by ',' ");
+    array=input.split(",").map(Number);
+    arrayDisplay.innerHTML=array.join(", ");
 })();
 
-console.log(array[3])
-console.log(type(array))
+
+maxiBtn.addEventListener("click",maxi);
+function maxi() {
+    let mx = -Infinity;
+
+    for (let x of array) {
+        if (mx < x) {
+            mx = x;
+        }
+    }
+
+    arrayDisplay.innerHTML = mx;
+}
+
+
+miniBtn.addEventListener("click",mini);
+
+function mini() {
+    let mi = Infinity;
+
+    for (let x of array) {
+        if (mi > x) {  
+            mi = x;
+        }
+    }
+
+    arrayDisplay.innerHTML = mi;
+}
+
+
+dRbutton.addEventListener("click",duplicatesRem);
+
+function duplicatesRem(){
+     let removed_duplicates = [...new Set(array)];
+    arrayDisplay.innerHTML = removed_duplicates.join(", ");
+}
+
+
+
